46f2460497f20361a26030048bb45078827d6e13
HaSeung2 <dlgktmd0@naver.com>
2025-03-01 19:50:14 +0900

    git init

---
+++ src/main/java/com/wheather/weathercodi/WeatherCodiApplication.java
@@ -0,0 +1,13 @@
+package com.wheather.weathercodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherCodiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherCodiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wheather/weathercodi/WeatherCodiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheather.weathercodi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherCodiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
